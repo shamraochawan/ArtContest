c1eab6e24a9d3e730875c9f66edb4153856818eb
shamraochawan <shamraochawan15@gmail.com>
2025-12-09 17:56:04 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,15 +2,16 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBJnQg1aQ9XX_0Y9QiUfs6qaHjIsP4LYtQ",
-  authDomain: "onlineartcontest-4c0a4.firebaseapp.com",
-  projectId: "onlineartcontest-4c0a4",
-  storageBucket: "onlineartcontest-4c0a4.firebasestorage.app",
-  messagingSenderId: "897533892711",
-  appId: "1:897533892711:web:12978a72f2c1f9776bdcc2",
-  measurementId: "G-NB8KSP7QRR"
+  apiKey: "AIzaSyBLmlRbxZI-6ONXS4PEUFMXCLUD-drBCTY",
+  authDomain: "art-contest-2025.firebaseapp.com",
+  projectId: "art-contest-2025",
+  storageBucket: "art-contest-2025.firebasestorage.app",
+  messagingSenderId: "523292533996",
+  appId: "1:523292533996:web:4ef1909f548458b6c1f1c9",
+  measurementId: "G-FNW5G94341"
 };
 
 const app = initializeApp(firebaseConfig);
